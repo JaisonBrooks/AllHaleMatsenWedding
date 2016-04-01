cfabc48f0090057c2c6186b098a4f34320cb1f29
Jaison Brooks <jbrooks@enphaseenergy.com>
2016-04-01 01:08:04 -0600

    improved nav links clicked css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 $(function() {
   // $('a[href*="#"]:not([href="#"])')
   $('.scroll').click(function() {
+    $('li.active').removeClass('active');
+    var li = $(this).parent();
+    if (!$(li).hasClass('active')) {
+      $(li).addClass('active');
+    }
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
